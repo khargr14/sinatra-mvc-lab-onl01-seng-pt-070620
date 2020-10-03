18dd3b1269bd6fe39ece5ca2934a733f7b7f14bc
Keesha Hargrove <kharg14@gmail.com>
2020-10-03 16:50:42 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,7 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  
+  get '/' do 
+        erb :user_input
+    end
+
+    post '/piglatinize' do
+        @p = PigLatinizer.new
+        erb :piglatinize
+    end
   
   
 end
